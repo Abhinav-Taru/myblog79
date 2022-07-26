6302a9d1333564d949875c5c7d993c7bbad96912
Abhinav-Taru <96816677+Abhinav-Taru@users.noreply.github.com>
2022-07-26 17:10:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-menu_list_array = ["Veg Margherita Pizza",
-                        "Chicken Tandoori Pizza",
-                        "Veg Supreme Pizza",
-                        "Paneer Tikka Pizza",
-                        "Deluxe Veggie Pizza",
-                        "Veg Extravaganza Pizza"];
-    
-    function getmenu(){
-        var htmldata="";
-        menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;
-        //give the appropriate id name as display_menu 
-    }
-
-    function add_item(){
-        var htmldata;
-		var imgtags='<img id="im1" src="pizzaImg.png">'
-        var item=document.getElementById("add_item").value;
-        // use the sort function as - menu_list_array.sort();
-        htmldata="";
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;
-		
-    }
-
-function add_top(){
-	var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
+student_array=[];
+function submit(){
+    var name_1=document.getElementById("name_of_student_1").value;
+    var name_2=document.getElementById("name_of_student_2").value;
+    var name_3=document.getElementById("name_of_student_3").value;
+    var name_4=document.getElementById("name_of_student_4").value;
+    student_array.push(name_1);
+    student_array.push(name_2);
+    student_array.push(name_3);
+    student_array.push(name_4);
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
 }
-
-function sort(){
-    menu_list_array.sort();
+function sorting(){
+    student_array.sort();
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
 }
